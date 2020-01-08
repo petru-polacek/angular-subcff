5f6d7c7d32dbc875c7df7fd4ccc3cab32e35e70c
petru-polacek <petru.polacek@gmail.com>
2020-01-08 23:46:11 +0100

    Change styles into styleUrls

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styleUrls: [ './hello.component.css' ]
+  // styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
   @Input() name: string;
